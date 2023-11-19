b9bd134ada0e89904868379baf6ef1a10541fcb8
rapha-pereira <raphaelpgomes1@gmail.com>
2023-11-19 16:50:05 -0300

    Refactor cache and queue services

---
+++ src/bot/services/botavio-queue.js
@@ -0,0 +1,36 @@
+/**
+ * Represents a BotavioQueue.
+ * @class
+ */
+class BotavioQueue {
+  /**
+   * Creates an instance of BotavioQueue.
+   * @constructor
+   */
+  constructor() {
+    this.lockService = LockService.getScriptLock();
+  }
+
+  /**
+   * Checks if the queue is free.
+   * @returns {boolean} Returns true if the queue is free, false otherwise.
+   */
+  isFreeQueue() {
+    let freeQueue = this.lockService.tryLock(30000);
+    
+    if (!freeQueue){
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Releases the queue lock.
+   * @returns {boolean} Returns true if the queue lock is successfully released.
+   */
+  releaseQueue() {
+    this.lockService.releaseLock();
+    return true;
+  }
+}
